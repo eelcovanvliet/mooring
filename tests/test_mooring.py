275df35ca985ad763527555de902252d743c34c6
eelco van Vliet <eelcovanvliet@gmail.com>
2023-06-29 12:25:00 +0200

    completed first test (currently fails)

--- tests/test_mooring.py
+++ tests/test_mooring.py
@@ -1,15 +1,15 @@
 
 import pytest
 from mooring.mooring import MooringCatenary
-from mooring.mooringdesigns import Catenary
-from tlp.designs import Site
+from mooring.mooringdesigns import CatenaryParameters
+from hivemind.site import Site, SiteParameters
 import OrcFxAPI
 
 def test_mooring():
-    site = Site()
-    installation = Catenary()
-    installation.MooringHorizontaLForceAtFloater['MN'] = 10
-    cat = MooringCatenary(site=site, installation=installation)
+    parameters = CatenaryParameters()
+    parameters.MooringHorizontaLForceAtFloater['MN'] = 25
+    parameters.RadiusAnchorPointWrtColumnAxis
+    cat = MooringCatenary(parameters=parameters)
 
     model = OrcFxAPI.Model()
     _ = model.CreateObject(OrcFxAPI.otVesselType)
@@ -17,11 +17,22 @@ def test_mooring():
     model.SaveData('test_mooring.dat')
 
     
+    anchor_point_radius = 300
     connection_points = [[0,5,0], [90,0,5], [180,-5,0], [270,0,-5]]
-    anchor_points = [[0,100,0], [90,0,100], [180,-100,0], [270,0,-100]]
-    cat.create_ofx(model, connection_points, anchor_points)
+    anchor_points = [
+        [0,anchor_point_radius,0], 
+        [90,0,anchor_point_radius], 
+        [180,-anchor_point_radius,0], 
+        [270,0,-anchor_point_radius],
+        ]
+    cat.create_in_ofx(model, connection_points, anchor_points)
 
     model.SaveData('test_mooring.dat')
+
+    model.CalculateStatics()
+    line = model['Line1']
+    GX_force = line.StaticResult('End GX force', OrcFxAPI.oeEndA) # kN
+    assert GX_force == 25e3
     
 
 
